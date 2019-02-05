54a3b747ec7571573bd0672b061e0289433ce17b
Patrice Breton <pbreton@apple.com>
2019-02-05 08:25:22 -0800

    update for 2.5

--- make-patch.sh
+++ make-patch.sh
@@ -4,5 +4,5 @@ PATCH_FILE=maas-vmware-fusion.patch
 
 for f in `find src -type f`; do
   diff -Naur ../maas/${f} ${f}
-  done | sed -e 's| .*src/| |' > ${PATCH_FILE}
+  done | sed -e 's| .*src/| |;s/from src./from /' > ${PATCH_FILE}
 
